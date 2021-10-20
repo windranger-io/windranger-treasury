2cbc80cba3b0861da67f36f541a0ef9b27eafe40
CJ <CjHare@users.noreply.github.com>
2021-10-20 18:17:02 +1000

    bond deposit

--- contracts/bond/Bond.sol
+++ contracts/bond/Bond.sol
@@ -1,4 +1,51 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.2;
 
-contract Bond {}
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+
+/**
+ * @title Bond contract that issues debt tokens in exchange for a security.
+ *
+ * @dev A single token type is held by the contract as security.
+ */
+contract Bond is Context, ERC20 {
+    event Deposit(address depositor, uint256 amount);
+    event DebtCertificate(address receiver, uint256 amount);
+
+    IERC20 private _securityToken;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        address securityToken
+    ) ERC20(name, bond) {
+        _securityToken = IERC20(securityToken);
+    }
+
+    /**
+     * @dev This contract must have been approved to transfer the given amount from the ERC20 token being used as security.
+     */
+    function deposit(uint256 amount) external {
+        require(amount <= totalSupply(), "Bond:deposit:Deposit too large");
+        address sender = _msgSender();
+
+        // Unknown ERC20 token behaviour, cater for bool usage
+        bool transferred = _securityToken.transferFrom(
+            sender,
+            address(this),
+            amount
+        );
+        require(transferred, "Bond::deposit:Security transfer failed");
+        emit Deposit(sender, amount);
+
+        transfer(sender, amount);
+        emit DebtCertificate(sender, amount);
+    }
+
+    //TODO override the mint operation with access control
+
+    //TODO redeem - use increaseAllowance
+    //TODO slash
+}
--- contracts/bond/BondFactory.sol
+++ contracts/bond/BondFactory.sol
@@ -1,4 +1,32 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
-contract BondFactory {}
+import "@openzeppelin/contracts/utils/Context.sol";
+import "./bond.sol";
+
+/**
+ * @title Creates configured bond contracts.
+ *
+ * @dev Applies common configuration to bond contract created.
+ */
+contract BondFactory is Context {
+    event BondCreated();
+
+    //TODO do we need a reference to each created bond in the factory?
+
+    constructor() {}
+
+    function createBond() external returns (address) {
+        address from = _msgSender();
+
+        //TODO init/create with config
+
+        Bond bond = new Bond();
+
+        emit BondCreated();
+
+        //TODO mint the initial supply of debt tokens
+
+        return bond;
+    }
+}
