f00ed9360bcc343b80fc52376833dd1eee4e1fa5
CJ <CjHare@users.noreply.github.com>
2021-10-20 22:07:00 +1000

    bond factory

--- contracts/bond/Bond.sol
+++ contracts/bond/Bond.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.2;
 
+import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-import "@openzeppelin/contracts/utils/Context.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 
 /**
  * @title Bond contract that issues debt tokens in exchange for a security.
