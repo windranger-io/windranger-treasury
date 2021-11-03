b4f241fe8a779ec0d4013f97721ac9fda809b745
CJ <CjHare@users.noreply.github.com>
2021-11-03 17:13:37 +1000

    feat: script to generate svg from puml input

--- scripts/plantuml.mjs
+++ scripts/plantuml.mjs
@@ -1,47 +1,59 @@
-import plantuml from 'node-plantuml'
-import fs from 'fs'
-import {readFile, mkdir} from 'fs/promises'
-import path from 'path'
+import plantuml from "node-plantuml";
+import fs from "fs";
+import { readdir, readFile, mkdir } from "fs/promises";
+import path from "path";
 
-const myArgs = process.argv.slice(2)
+const myArgs = process.argv.slice(2);
+const plantUmlExtension = ".puml";
 
 for (const arg of myArgs) {
-    const pathObj = path.parse(arg)
+  const targetFile = path.parse(arg);
+  validatePlantUmlExtensionType(targetFile.ext);
 
-    const content = await readFile(arg, 'utf8')
-    const len = (content.match(/newpage/g) || []).length
+  const content = await readFile(arg, "utf8");
+  validateNonEmptyContent(content, arg);
 
-    const targetDirectory =
-        pathObj.dir + path.sep + 'images' + path.sep + pathObj.name + path.sep
+  const len = (content.match(/newpage/g) || []).length;
 
-    try {
-        await mkdir(targetDirectory, {recursive: true})
-    } catch (err) {
-        //  nop if directory doesn't exist
-    }
+  await createDirectoryStructure(targetFile);
+  const targetDirectory = targetFile.dir + path.sep + "images" + path.sep;
 
-    //TODO code me - get from the arg, regex for name - extension & before last slash
-    const prefix = 'myPrefix';
-
-    for (let i = 0; i <= len; i++) {
-        await generatePage(content, i, targetDirectory, prefix)
-    }
+  for (let i = 0; i <= len; i++) {
+    await generatePage(content, i, targetDirectory, targetFile.name);
+  }
 }
 
-async function generatePage(content, page, targetDirectory, targetPrefix){
-    const generated = plantuml.generate(content, {format: 'png', pipeimageindex: page})
+async function generatePage(content, page, targetDirectory, targetNamePrefix) {
+  const generated = plantuml.generate(content, {
+    format: "svg",
+    pipeimageindex: page,
+  });
 
-    const outputFile =
-      targetDirectory + targetPrefix + '-plantuml-image-' + page + '.png'
+  const outputFile = targetDirectory + targetNamePrefix + "-" + page + ".svg";
 
-    console.log('starting:' + outputFile)
+  console.log("Begun generation of " + outputFile);
 
-    console.log(content.length)
+  generated.out.pipe(fs.createWriteStream(outputFile)).on("finish", (_) => {
+    console.log("Finished generation of " + outputFile);
+  });
+}
 
-    generated.out.pipe(fs.createWriteStream(outputFile)).on('finish', (_) => {
-        console.log(
-          'generated image ' +
-          outputFile
-        )
-    })
+async function createDirectoryStructure(target) {
+  try {
+    await mkdir(target, { recursive: true });
+  } catch (err) {
+    //  nop if directory doesn't exist
+  }
+}
+
+function validatePlantUmlExtensionType(extension) {
+  if (plantUmlExtension !== extension.toLowerCase()) {
+    throw Error("Expecting a .puml extension, instead got: " + extension);
+  }
+}
+
+function validateNonEmptyContent(content, filepath) {
+  if (content.length === 0) {
+    throw Error("Content must not be empty, however not true for " + filepath);
+  }
 }
