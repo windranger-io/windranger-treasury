80431eeb0e29c8f387b2443394a84a4902850960
CJ <CjHare@users.noreply.github.com>
2021-11-08 20:17:18 +1000

    fix: zero address check on parameters

--- contracts/bond/BondFactory.sol
+++ contracts/bond/BondFactory.sol
@@ -22,8 +22,16 @@ contract BondFactory is Context, Ownable {
     address private _collateralToken;
     address private _treasury;
 
-    constructor(address securityToken_, address treasury_) {
-        _collateralToken = securityToken_;
+    constructor(address collateralToken_, address treasury_) {
+        require(
+            collateralToken_ != address(0),
+            "BondFactory::constructor: treasury cannot be zero address"
+        );
+        require(
+            treasury_ != address(0),
+            "BondFactory::constructor: collateral token cannot be zero address"
+        );
+        _collateralToken = collateralToken_;
         _treasury = treasury_;
     }
 
@@ -47,21 +55,6 @@ contract BondFactory is Context, Ownable {
     }
 
     /**
-     * @dev Retrieves the address that receives any slashed or remaining funds.
-     */
-    function treasury() external view returns (address) {
-        return _treasury;
-    }
-
-    /**
-     * @dev Permits the owner to update the treasury address.
-     * Only applies for bonds created after the update, previously created bonds remain unchanged.
-     */
-    function setTreasury(address treasury_) external onlyOwner {
-        _treasury = treasury_;
-    }
-
-    /**
      * @dev Retrieves the address for the collateral token.
      */
     function collateralToken() external view returns (address) {
@@ -69,10 +62,33 @@ contract BondFactory is Context, Ownable {
     }
 
     /**
+     * @dev Retrieves the address that receives any slashed or remaining funds.
+     */
+    function treasury() external view returns (address) {
+        return _treasury;
+    }
+
+    /**
      * @dev Permits the owner to update the collateral token address.
      * Only applies for bonds created after the update, previously created bonds remain unchanged.
      */
     function setCollateralToken(address collateralToken_) external onlyOwner {
+        require(
+            collateralToken_ != address(0),
+            "BondFactory::setCollateralToken: treasury cannot be zero address"
+        );
         _collateralToken = collateralToken_;
+    }
+
+    /**
+     * @dev Permits the owner to update the treasury address.
+     * Only applies for bonds created after the update, previously created bonds remain unchanged.
+     */
+    function setTreasury(address treasury_) external onlyOwner {
+        require(
+            treasury_ != address(0),
+            "BondFactory::setTreasury: collateral token cannot be zero address"
+        );
+        _treasury = treasury_;
     }
 }
