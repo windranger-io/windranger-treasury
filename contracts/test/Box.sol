71c89f6db6a0969809bdc44da17eabea3c050db4
CJ <CjHare@users.noreply.github.com>
2021-12-10 09:54:35 +1000

    doco

--- contracts/test/Box.sol
+++ contracts/test/Box.sol
@@ -14,7 +14,7 @@ contract Box {
     /**
         @notice store the given value.
         @dev storing the value causes the Store event to be emitted, overwriting any previously stored value.
-        @param boxValue_ value for storage in the Box, no restrictions.
+        @param boxValue value for storage in the Box, no restrictions.
      */
     function store(string calldata boxValue) external {
         _value = boxValue;
