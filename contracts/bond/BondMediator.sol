35a81efe146449f2d9f33b021d12212c828bae8e
CJ <CjHare@users.noreply.github.com>
2021-12-17 20:36:57 +1000

    BondMediator

--- contracts/bond/BondMediator.sol
+++ contracts/bond/BondMediator.sol
@@ -1,26 +1,39 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "./BondCreator.sol";
 import "./BondCurator.sol";
 
 /**
- * @title Manages bond contracts.
+ * @title Mediates between a Bond creator and Bond curator.
  *
- * @dev Store for common configuration and managing Bond contracts.
+ * @dev Orchestrates a BondCreator and BondCurator to provide a single function to aggregate the various calls
+ *      providing a single function to create and setup a bond for management with the curator.
  */
-contract BondSetupMediator is OwnableUpgradeable, UUPSUpgradeable {
+contract BondMediator is OwnableUpgradeable, UUPSUpgradeable {
     BondCreator private _creator;
     BondCurator private _curator;
 
+    /**
+     * @param factory A deployed BondCreator contract to use when creating bonds.
+     * @param manager A deployed BondCurator contract to register created bonds with,
+     */
     function initialize(address factory, address manager)
         external
         virtual
         initializer
     {
-        //TODO check contract addresses
+        require(
+            AddressUpgradeable.isContract(factory),
+            "Mediator: creator not a contract"
+        );
+        require(
+            AddressUpgradeable.isContract(manager),
+            "Mediator: curator not a contract"
+        );
 
         __Ownable_init();
 
@@ -28,6 +41,9 @@ contract BondSetupMediator is OwnableUpgradeable, UUPSUpgradeable {
         _curator = BondCurator(manager);
     }
 
+    /**
+     * @notice Creates a Bond from the BondCreator and registers it with the BondCurator.
+     */
     function createManagedBond(
         string calldata name,
         string calldata symbol,
@@ -52,6 +68,14 @@ contract BondSetupMediator is OwnableUpgradeable, UUPSUpgradeable {
         _curator.addBond(bond);
 
         return bond;
+    }
+
+    function bondCreator() external view returns (address) {
+        return _creator;
+    }
+
+    function bondCurator() external view returns (address) {
+        return _curator;
     }
 
     /**
