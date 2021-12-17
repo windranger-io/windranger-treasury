a17982da4d8d2747b796a352b6cedab15ee46620
CJ <CjHare@users.noreply.github.com>
2021-12-17 22:13:03 +1000

    removing collateral whitelist from manager

--- contracts/bond/BondManager.sol
+++ contracts/bond/BondManager.sol
@@ -14,19 +14,12 @@ import "./BondCurator.sol";
  *
  * @dev Owns of all Bonds it manages, guarding function accordingly allows finer access control to be provided.
  */
-contract BondManager is
-    BondCurator,
-    CollateralWhitelist,
-    OwnableUpgradeable,
-    UUPSUpgradeable
-{
+contract BondManager is BondCurator, OwnableUpgradeable, UUPSUpgradeable {
     using EnumerableSetUpgradeable for EnumerableSetUpgradeable.AddressSet;
 
     EnumerableSetUpgradeable.AddressSet private _bonds;
 
     function addBond(address bond) external override {
-        //TODO require collateral is in whitelist
-
         //TODO event
 
         bool added = _bonds.add(bond);
@@ -38,63 +31,8 @@ contract BondManager is
         );
     }
 
-    //TODO update doco
-    //TODO update messagegs
-
-    /**
-     * @notice Initialises with the given collateral tokens automatically being whitelisted.
-     *
-     * @param erc20CollateralTokens Collateral token contract. Must not be address zero.
-     */
-    function initialize(address erc20CollateralTokens)
-        external
-        virtual
-        initializer
-    {
-        __BondManager_init(erc20CollateralTokens);
-    }
-
-    /**
-     * @notice Permits the owner to update the address of already whitelisted collateral token.
-     *
-     * @dev Only applies for bonds created after the update, previously created bonds remain unchanged.
-     *
-     * @param erc20CollateralTokens Must already be whitelisted and must not be address zero.
-     */
-    function updateWhitelistedCollateral(address erc20CollateralTokens)
-        external
-        onlyOwner
-    {
-        _updateWhitelistedCollateral(erc20CollateralTokens);
-    }
-
-    /**
-     * @notice Permits the owner to remove a collateral token from being accepted in future bonds.
-     *
-     * @dev Only applies for bonds created after the removal, previously created bonds remain unchanged.
-     *
-     * @param symbol Symbol must exist in the collateral whitelist.
-     */
-    function removeWhitelistedCollateral(string calldata symbol)
-        external
-        onlyOwner
-    {
-        _removeWhitelistedCollateral(symbol);
-    }
-
-    /**
-     * @notice Adds an ERC20 token to the collateral whitelist.
-     *
-     * @dev When a bond is created, the tokens used as collateral must have been whitelisted.
-     *
-     * @param erc20CollateralTokens Whitelists the token from now onwards.
-     *      On bond creation the tokens address used is retrieved by symbol from the whitelist.
-     */
-    function whitelistCollateral(address erc20CollateralTokens)
-        external
-        onlyOwner
-    {
-        _whitelistCollateral(erc20CollateralTokens);
+    function initialize() external virtual initializer {
+        __Ownable_init();
     }
 
     function bondAt(uint256 index) external view returns (address) {
@@ -110,15 +48,7 @@ contract BondManager is
         return EnumerableSetUpgradeable.length(_bonds);
     }
 
-    function __BondManager_init(address erc20CollateralTokens)
-        internal
-        onlyInitializing
-    {
-        __Ownable_init();
-        __CollateralWhitelist_init();
-
-        _whitelistCollateral(erc20CollateralTokens);
-    }
+    //TODO proxy through bond operations
 
     /**
      * @notice Permits only the owner to perform proxy upgrades.
