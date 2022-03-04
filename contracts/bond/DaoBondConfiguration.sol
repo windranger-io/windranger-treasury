7ca8184d672927e172b284c9bc41368395c4c58b
Aodhgan Gleeson <--global>
2022-03-04 15:25:47 -0800

    refactor common daoId

--- contracts/bond/DaoBondConfiguration.sol
+++ contracts/bond/DaoBondConfiguration.sol
@@ -2,20 +2,20 @@
 pragma solidity ^0.8.0;
 
 import "./DaoBondCollateralWhitelist.sol";
+import "../Dao.sol";
 
-abstract contract DaoBondConfiguration is DaoBondCollateralWhitelist {
+abstract contract DaoBondConfiguration is DaoBondCollateralWhitelist, Dao {
     struct DaoBondConfig {
         // Address zero is an invalid address, can be used to identify null structs
-        address treasury;
+        // address treasury;
+
+        DaoConfig dao;
         CollateralWhitelist whitelist;
     }
 
-    mapping(uint256 => DaoBondConfig) private _daoConfig;
-    uint256 private _daoConfigLastId;
+    mapping(uint256 => DaoBondConfig) private _daoBondConfig;
 
-    function daoTreasury(uint256 daoId) external view returns (address) {
-        return _daoConfig[daoId].treasury;
-    }
+    // uint256 private _daoConfigLastId;
 
     /**
      * @notice The _msgSender() is given membership of all roles, to allow granting and future renouncing after others
@@ -30,36 +30,12 @@ abstract contract DaoBondConfiguration is DaoBondCollateralWhitelist {
         internal
         returns (uint256)
     {
-        require(
-            erc20CapableTreasury != address(0),
-            "DAO Treasury: address is zero"
-        );
+        uint256 daoId = _daoConfiguration(erc20CapableTreasury);
 
-        _daoConfigLastId++;
+        DaoBondConfig storage config = _daoBondConfig[daoId];
+        config.dao.treasury = erc20CapableTreasury;
 
-        DaoBondConfig storage config = _daoConfig[_daoConfigLastId];
-        config.treasury = erc20CapableTreasury;
-
-        return _daoConfigLastId;
-    }
-
-    function _daoTreasury(uint256 daoId) internal returns (address) {
-        return _daoConfig[daoId].treasury;
-    }
-
-    function _setDaoTreasury(uint256 daoId, address replacementTreasury)
-        internal
-    {
-        require(_isValidDaoId(daoId), "DAO Treasury: invalid DAO Id");
-        require(
-            replacementTreasury != address(0),
-            "DAO Treasury: address is zero"
-        );
-        require(
-            _daoConfig[daoId].treasury != replacementTreasury,
-            "DAO Treasury: identical address"
-        );
-        _daoConfig[daoId].treasury = replacementTreasury;
+        return daoId;
     }
 
     function _daoCollateralWhitelist(uint256 daoId)
@@ -68,15 +44,6 @@ abstract contract DaoBondConfiguration is DaoBondCollateralWhitelist {
         override
         returns (CollateralWhitelist storage)
     {
-        return _daoConfig[daoId].whitelist;
-    }
-
-    function _isValidDaoId(uint256 daoId)
-        internal
-        view
-        override
-        returns (bool)
-    {
-        return _daoConfig[daoId].treasury != address(0);
+        return _daoBondConfig[daoId].whitelist;
     }
 }
