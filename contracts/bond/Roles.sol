2c99ea8a24a44744a27b5f7c909dfd0b8c12ec6a
CJ <CjHare@users.noreply.github.com>
2022-03-08 14:01:07 +1000

    feat: permissioning

--- contracts/bond/Roles.sol
+++ contracts/bond/Roles.sol
@@ -6,12 +6,16 @@ pragma solidity ^0.8.0;
  *
  * @notice The DAO grants all other roles (role admin for all roles), while not permitted any actions itself.
  *
+ *  SUPER_USER role the manage for DAO_CREATOR roles, in addition to being a super set to to all other roles functions.
+ *  DAO_CREATOR role only business is creating DAOs and their configurations.
+ *  DAO_ADMIN role can update the DAOs configuration and may intervene to sweep / flush Bonds.
  *  BOND_ADMIN role is deals with the Bond life cycle.
- *  BOND_AGGREGATOR roles collates bonds together in one place.
  *  SYSTEM_ADMIN role deals with upgrading the contract.
  */
 library Roles {
+    bytes32 public constant SUPER_USER = "SUPER_USER";
     bytes32 public constant DAO_ADMIN = "DAO_ADMIN";
+    bytes32 public constant DAO_ADMIN = "DAO_CREATOR";
     bytes32 public constant BOND_ADMIN = "BOND_ADMIN";
     bytes32 public constant SYSTEM_ADMIN = "SYSTEM_ADMIN";
 }
