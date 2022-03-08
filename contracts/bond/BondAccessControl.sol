439ecd8defa61575fe5788c6caf471dd90321e9d
CJ <CjHare@users.noreply.github.com>
2022-03-08 16:37:58 +1000

    roles

--- contracts/bond/BondAccessControl.sol
+++ contracts/bond/BondAccessControl.sol
@@ -10,6 +10,17 @@ import "./Roles.sol";
  * @notice Provides modifiers and management for access control required throughout the Bond contracts.
  */
 abstract contract BondAccessControl is AccessControlUpgradeable {
+    //    bytes32 public constant SUPER_USER = "SUPER_USER";
+    //    bytes32 public constant DAO_ADMIN = "DAO_ADMIN";
+    //    bytes32 public constant DAO_CREATOR = "DAO_CREATOR";
+    //    bytes32 public constant DAO_MEEPLE = "DAO_MEEPLE";
+    //    bytes32 public constant SYSTEM_ADMIN = "SYSTEM_ADMIN";
+
+    modifier hasRole() {
+        // TODO code
+        _;
+    }
+
     /**
      * @notice The _msgSender() is given membership of all roles, to allow granting and future renouncing after others
      *      have been setup.
@@ -18,11 +29,26 @@ abstract contract BondAccessControl is AccessControlUpgradeable {
     function __BondAccessControl_init() internal onlyInitializing {
         __AccessControl_init();
 
-        _setRoleAdmin(Roles.BOND_ADMIN, Roles.DAO_ADMIN);
+        /*
+         * Super Users administrate themselves, DAO Creators and SysAdmins.
+         */
+        _setRoleAdmin(Roles.SUPER_USER, Roles.SUPER_USER);
+        _setRoleAdmin(Roles.DAO_CREATOR, Roles.SUPER_USER);
+        _setRoleAdmin(Roles.SYSTEM_ADMIN, Roles.SUPER_USER);
+
         _setRoleAdmin(Roles.DAO_ADMIN, Roles.DAO_ADMIN);
-        _setRoleAdmin(Roles.SYSTEM_ADMIN, Roles.DAO_ADMIN);
+        _setRoleAdmin(Roles.BOND_ADMIN, Roles.DAO_ADMIN);
+
         _setupRole(Roles.BOND_ADMIN, _msgSender());
         _setupRole(Roles.DAO_ADMIN, _msgSender());
         _setupRole(Roles.SYSTEM_ADMIN, _msgSender());
     }
+
+    // TODO update doco - tiered access control
+
+    // TOOD restrict DAO admins & DAO Meeple to their DAOs
+
+    // TODO single role admin?
+
+    // TODO explicit modifiers
 }
