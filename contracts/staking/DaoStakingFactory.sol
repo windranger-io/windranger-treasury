d44def9318ed095867d9eebb1964f19d60f4fbca
Aodhgan Gleeson <--global>
2022-03-11 11:13:32 -0800

    remove factories

--- contracts/staking/DaoStakingFactory.sol
+++ contracts/staking/DaoStakingFactory.sol
@@ -1,16 +1,15 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
-import "./FixedStakingPoolFactory.sol";
-import "./FloatingStakingPoolFactory.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+
+import "./FixedStakingPool.sol";
+import "./FloatingStakingPool.sol";
+
 import "./StakingPoolInfo.sol";
 import "../RoleAccessControl.sol";
 
-contract DaoStakingFactory is
-    RoleAccessControl,
-    FixedStakingPoolFactory,
-    FloatingStakingPoolFactory
-{
+contract DaoStakingFactory is RoleAccessControl, PausableUpgradeable {
     function pause() external whenNotPaused atLeastSysAdminRole {
         _pause();
     }
@@ -19,29 +18,60 @@ contract DaoStakingFactory is
         _unpause();
     }
 
-    function initialize() public {
-        __FixedStakingPoolFactory_init();
-        __FloatingStakingPoolFactory_init();
-        __RoleAccessControl_init();
-    }
-
     function createFixedStakingPool(
         uint256 daoId,
         StakingPoolInfo.StakingPoolData calldata _info
-    ) public override atLeastDaoAminRole(daoId) returns (address) {
-        return super.createFixedStakingPool(daoId, _info);
+    ) external atLeastDaoAminRole(daoId) returns (address) {
+        FixedStakingPool fixedStakingPool = new FixedStakingPool();
+
+        emit StakingPoolInfo.StakingPoolCreated(
+            address(fixedStakingPool),
+            _info.treasury,
+            _msgSender(),
+            _info.rewardTokens,
+            address(_info.stakeToken),
+            _info.epochStartTimestamp,
+            _info.epochDuration,
+            _info.minimumContribution,
+            StakingPoolInfo.StakingPoolType.FIXED
+        );
+
+        fixedStakingPool.initialize(_info);
+
+        return address(fixedStakingPool);
     }
 
     function createFloatingStakingPool(
         uint256 daoId,
         StakingPoolInfo.StakingPoolData calldata _info
-    ) public override atLeastDaoAminRole(daoId) returns (address) {
-        return super.createFloatingStakingPool(daoId, _info);
+    ) external atLeastDaoAminRole(daoId) returns (address) {
+        FloatingStakingPool floatingStakingPool = new FloatingStakingPool();
+
+        emit StakingPoolInfo.StakingPoolCreated(
+            address(floatingStakingPool),
+            _info.treasury,
+            _msgSender(),
+            _info.rewardTokens,
+            address(_info.stakeToken),
+            _info.epochStartTimestamp,
+            _info.epochDuration,
+            _info.minimumContribution,
+            StakingPoolInfo.StakingPoolType.FLOATING
+        );
+
+        floatingStakingPool.initialize(_info);
+
+        return address(floatingStakingPool);
+    }
+
+    function initialize() public {
+        __Pausable_init();
+        __RoleAccessControl_init();
     }
 
     function _authorizeUpgrade(address newImplementation)
         internal
-        override
+        virtual
         atLeastSysAdminRole
     {}
 }
