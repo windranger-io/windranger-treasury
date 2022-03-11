d6b5ecdd633150b6ceff88059c15b0ea46e56856
Aodhgan Gleeson <--global>
2022-03-11 11:13:32 -0800

    remove factories

--- contracts/staking/StakingPoolInfo.sol
+++ contracts/staking/StakingPoolInfo.sol
@@ -3,6 +3,11 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 library StakingPoolInfo {
+    enum StakingPoolType {
+        FIXED,
+        FLOATING
+    }
+
     struct RewardToken {
         address rewardToken;
         uint32 rewardAmountRatio; // denominated in fixed point ETHER
@@ -15,11 +20,22 @@ library StakingPoolInfo {
         uint128 minimumContribution;
         uint32 epochDuration;
         uint32 epochStartTimestamp;
-        uint32 rewardUnlockTimestamp;
         bool rewardsFinalized;
         address treasury;
         uint128 totalStakedAmount;
         IERC20 stakeToken;
         RewardToken[] rewardTokens;
     }
+
+    event StakingPoolCreated(
+        address indexed stakingPool,
+        address treasury,
+        address indexed creator,
+        StakingPoolInfo.RewardToken[] rewardTokens,
+        address stakeToken,
+        uint128 epochStartTimestamp,
+        uint128 epochDuration,
+        uint128 minimumContribution,
+        StakingPoolType stakingPoolType
+    );
 }
