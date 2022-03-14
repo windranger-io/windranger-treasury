2256de8af2b95b239f10506d88573508b918cc2e
Aodhgan Gleeson <--global>
2022-03-14 11:38:52 -0700

    add initializer modifier

--- contracts/staking/DaoStakingFactory.sol
+++ contracts/staking/DaoStakingFactory.sol
@@ -2,10 +2,10 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 import "./FixedStakingPool.sol";
 import "./FloatingStakingPool.sol";
-
 import "./StakingPoolInfo.sol";
 import "../RoleAccessControl.sol";
 
@@ -20,51 +20,51 @@ contract DaoStakingFactory is RoleAccessControl, PausableUpgradeable {
 
     function createFixedStakingPool(
         uint256 daoId,
-        StakingPoolInfo.StakingPoolData calldata _info
+        StakingPoolInfo.StakingPoolData calldata info
     ) external atLeastDaoAminRole(daoId) returns (address) {
         FixedStakingPool fixedStakingPool = new FixedStakingPool();
 
         emit StakingPoolInfo.StakingPoolCreated(
             address(fixedStakingPool),
-            _info.treasury,
+            info.treasury,
             _msgSender(),
-            _info.rewardTokens,
-            address(_info.stakeToken),
-            _info.epochStartTimestamp,
-            _info.epochDuration,
-            _info.minimumContribution,
+            info.rewardTokens,
+            address(info.stakeToken),
+            info.epochStartTimestamp,
+            info.epochDuration,
+            info.minimumContribution,
             StakingPoolInfo.StakingPoolType.FIXED
         );
 
-        fixedStakingPool.initialize(_info);
+        fixedStakingPool.initialize(info);
 
         return address(fixedStakingPool);
     }
 
     function createFloatingStakingPool(
         uint256 daoId,
-        StakingPoolInfo.StakingPoolData calldata _info
+        StakingPoolInfo.StakingPoolData calldata info
     ) external atLeastDaoAminRole(daoId) returns (address) {
         FloatingStakingPool floatingStakingPool = new FloatingStakingPool();
 
         emit StakingPoolInfo.StakingPoolCreated(
             address(floatingStakingPool),
-            _info.treasury,
+            info.treasury,
             _msgSender(),
-            _info.rewardTokens,
-            address(_info.stakeToken),
-            _info.epochStartTimestamp,
-            _info.epochDuration,
-            _info.minimumContribution,
+            info.rewardTokens,
+            address(info.stakeToken),
+            info.epochStartTimestamp,
+            info.epochDuration,
+            info.minimumContribution,
             StakingPoolInfo.StakingPoolType.FLOATING
         );
 
-        floatingStakingPool.initialize(_info);
+        floatingStakingPool.initialize(info);
 
         return address(floatingStakingPool);
     }
 
-    function initialize() external {
+    function initialize() external initializer {
         __Pausable_init();
         __RoleAccessControl_init();
     }
