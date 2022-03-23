f79a26abc10dbf07f554a6c5f9975f7ab1cccfc5
Aodhgan Gleeson <--global>
2022-03-23 13:08:30 -0700

    staking pool verification code

---
+++ test/contracts/staking/staking-factory-events.ts
@@ -0,0 +1,44 @@
+import {BigNumber, Event} from 'ethers'
+import {StakingPoolCreatedEvent} from '../../../typechain-types/StakingPoolFactory'
+import {expect} from 'chai'
+import {StakingPoolType} from './staking-events'
+
+type RewardToken = {
+    token: string
+}
+
+export type ActualStakingPoolCreatedEvent = {
+    // stakingPool: string
+    treasury: string
+    creator: string
+    rewardTokens: RewardToken[]
+    stakeToken: string
+    epochStartTimestamp: BigNumber
+    epochDuration: BigNumber
+    minimumContribution: BigNumber
+    stakingPoolType: StakingPoolType
+}
+
+/**
+ * Shape check and conversion for a StakingPoolCreated
+ */
+export function stakingPoolCreated(
+    event: Event
+): ActualStakingPoolCreatedEvent {
+    const created = event as StakingPoolCreatedEvent
+    expect(event.args).is.not.undefined
+
+    const args = event.args
+
+    // expect(args?.stakingPool).is.not.undefined
+    expect(args?.treasury).is.not.undefined
+    expect(args?.creator).is.not.undefined
+    expect(args?.rewardTokens).is.not.undefined
+    expect(args?.stakeToken).is.not.undefined
+    expect(args?.epochStartTimestamp).is.not.undefined
+    expect(args?.epochDuration).is.not.undefined
+    expect(args?.minimumContribution).is.not.undefined
+    expect(args?.stakingPoolType).is.not.undefined
+
+    return created.args
+}
