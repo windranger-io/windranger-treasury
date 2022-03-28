b145e854ffb57f817daa0c0a1cac64080686fbf1
Aodhgan Gleeson <--global>
2022-03-28 14:10:21 -0700

    more merging

--- contracts/bond/BondFactory.sol
+++ contracts/bond/BondFactory.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "./SingleCollateralMultiRewardBond.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 import "./SingleCollateralMultiRewardBond.sol";
