2771d25d4fa92a0843296019d91ed3a24d745841
Aodhgan Gleeson <--global>
2022-03-28 14:33:50 -0700

    building

--- contracts/bond/BondFactory.sol
+++ contracts/bond/BondFactory.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "./SingleCollateralMultiRewardBond.sol";
-import "./RoleAccessControl.sol";
+import "../RoleAccessControl.sol";
 import "./BondCreator.sol";
 import "../Roles.sol";
 import "../Version.sol";
